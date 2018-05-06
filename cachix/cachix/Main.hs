44f9f5a9e2b0e3b2889b60e7d61cc57e0ebadb5a
Domen Kožar <domen@dev.si>
2018-05-06 13:54:44 +0200

    main: use utf-8 and handle exceptions

--- cachix/cachix/Main.hs
+++ cachix/cachix/Main.hs
@@ -1,12 +1,25 @@
 module Main (main) where
 
+import Control.Exception (handle, SomeException)
 import System.IO
+import System.Exit (exitFailure)
+import GHC.IO.Encoding
 
 import qualified Cachix.Client as CC
 
 
 main :: IO ()
 main = do
+  setLocaleEncoding utf8
   hSetBuffering stdout LineBuffering
   hSetBuffering stderr LineBuffering
-  CC.main
+  handleExceptions CC.main
+
+handleExceptions :: IO a -> IO a
+handleExceptions = handle handler
+  where
+    handler :: SomeException -> IO a
+    handler e = do
+      hPutStrLn stderr ""
+      hPrint stderr e
+      exitFailure

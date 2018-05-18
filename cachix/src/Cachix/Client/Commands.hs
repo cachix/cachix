4d2cf6f2b578ce1f4e640d6303a20d7277194fb4
Domen Kožar <domen@dev.si>
2018-05-18 23:09:34 +0200

    Handle empty list of references

--- cachix/src/Cachix/Client/Commands.hs
+++ cachix/src/Cachix/Client/Commands.hs
@@ -211,7 +211,7 @@ sync env (Just Config{..}) name rawPaths = do
         (exitcode, out, err) <- readProcessWithExitCode "nix-store" ["-q", "--references", toS storePath] mempty
 
         let (storeHash, storeSuffix) = splitStorePath $ toS storePath
-            references = T.lines $ toS out
+            references = T.lines $ T.strip $ toS out
             fp = fingerprint storePath narHash narSize references
             sig = dsign sk fp
             nic = Api.NarInfoCreate

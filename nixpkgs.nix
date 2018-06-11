f4e4740d25ac769c092d37f86e7ceab38b80a675
Domen Kožar <domen@dev.si>
2018-06-11 15:06:06 +0200

    Bump nixpkgs

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,5 +1,5 @@
 # to update: $ nix-prefetch-url --unpack url
 import (builtins.fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/b88d66ce058b5f2894aa08ea4e97d3f35e134837.tar.gz";
-  sha256 = "108yz2jamx29zijcxkj1s98816sj3khrp3wgxvpny7sf9izfky15";
+  url = "https://github.com/nixos/nixpkgs/archive/338b407b27df2be760690c52fc0f13de0312504d.tar.gz";
+  sha256 = "1haf6rhrc9apzbkfmxqpqm6a6l3y85j9rfr44pwsazjpafm0pj7m";
 }) { config = {}; overlays = []; }

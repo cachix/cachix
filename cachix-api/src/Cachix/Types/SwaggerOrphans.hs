4bc7c1da8810c99b4289d6ce098e23184321a6ad
Domen Kožar <domen@dev.si>
2018-11-27 11:13:46 +0000

    For swagger2 2.3.1 we don't need the orphan anymore

--- cachix-api/src/Cachix/Types/SwaggerOrphans.hs
+++ cachix-api/src/Cachix/Types/SwaggerOrphans.hs
@@ -1,5 +1,6 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
@@ -15,10 +16,11 @@ import Servant.Streaming
 import Servant.Auth.Swagger ()
 import Web.Cookie           (SetCookie)
 
-
--- TODO: https://github.com/haskell-servant/servant-auth/pull/42#issuecomment-381279499
+#if MIN_VERSION_swagger2(2,3,1)
+#else
 instance ToParamSchema SetCookie where
-  toParamSchema _ = mempty -- TODO: cookie instances for swagger
+  toParamSchema _ = mempty
+#endif
 
 -- https://github.com/plow-technologies/servant-streaming/blob/master/servant-streaming-docs/src/Servant/Streaming/Docs/Internal.hs
 -- TODO: these should define the body/response content

3b277d960dc4dc8fda59de093640b792a6492c32
Domen Kožar <domen@dev.si>
2019-08-05 11:58:33 +0200

    LTS-13.30

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -5,6 +5,6 @@ let
     then builtins.fetchTarball { inherit url; }
     else builtins.fetchTarball attrs;
 in import (fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/d6b74a4145c28f9740c3147c588b122533fcd8f3.tar.gz";
-  sha256 = "1qy81fv5drkcdg3830hz17vf4xgkyxc93wm5m2744dyzzkj8fzp7";
+  url = "https://github.com/NixOS/nixpkgs/archive/981fdb42074fe961b30624b3b738e3c77a2d0a96.tar.gz";
+  sha256 = "1fnqscn89w0kfprjy60n696ckw866vwk2x43alylqn0yx3n6556w";
 }) { config = {}; overlays = []; }

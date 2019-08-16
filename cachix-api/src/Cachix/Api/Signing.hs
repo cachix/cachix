ca758a1e3f788b52aedfe2dd61090e9f096c282d
Domen Kožar <domen@dev.si>
2019-08-16 10:56:23 +0200

    Avoid another shell call when pushing

--- cachix-api/src/Cachix/Api/Signing.hs
+++ cachix-api/src/Cachix/Api/Signing.hs
@@ -1,15 +1,16 @@
 module Cachix.Api.Signing
   ( fingerprint
   , passthroughSizeSink
+  , passthroughHashSinkB16
   , passthroughHashSink
   ) where
 
 import           Crypto.Hash
-import           Control.Monad.IO.Class (MonadIO, liftIO)
-import qualified Data.ByteArray                as BA
+import           Control.Monad.IO.Class  (MonadIO, liftIO)
+import qualified Data.ByteArray as BA
+import           Data.ByteArray.Encoding (convertToBase, Base(..))
 import qualified Data.ByteString as BS
 import           Data.ByteString (ByteString)
-import qualified Data.ByteString.Base16        as B16
 import           Data.Conduit
 import qualified Data.Conduit.Combinators      as CC
 import           Data.IORef
@@ -35,8 +36,11 @@ hashSink = CC.foldM (\p n -> return (hashUpdate p n)) hashInit
 passthroughSizeSink :: MonadIO m => IORef Integer -> ConduitT ByteString ByteString m ()
 passthroughSizeSink ioref = passthroughSink sizeSink (liftIO . writeIORef ioref)
 
-passthroughHashSink :: MonadIO m => IORef Text -> ConduitT ByteString ByteString m ()
-passthroughHashSink ioref = passthroughSink hashSink (liftIO . writeIORef ioref . transf)
-  where
-    -- TODO: use cryptonite B16 to get rid of extra dep and simplify
-    transf = toS . B16.encode . BS.pack . BA.unpack . hashFinalize
+passthroughHashSinkBase :: MonadIO m => (Digest SHA256 -> ByteString) -> IORef ByteString -> ConduitT ByteString ByteString m ()
+passthroughHashSinkBase f ioref = passthroughSink hashSink (liftIO . writeIORef ioref . f . hashFinalize)
+
+passthroughHashSink :: MonadIO m => IORef ByteString -> ConduitT ByteString ByteString m ()
+passthroughHashSink = passthroughHashSinkBase BA.convert
+
+passthroughHashSinkB16 :: MonadIO m => IORef ByteString -> ConduitT ByteString ByteString m ()
+passthroughHashSinkB16 = passthroughHashSinkBase (convertToBase Base16)

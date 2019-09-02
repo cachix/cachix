4cda0100714d9032f4480cad882857803742a728
Domen Kožar <domen@dev.si>
2019-09-02 14:16:31 +0200

    Pretty print only cachix exceptions

--- cachix/cachix/Main.hs
+++ cachix/cachix/Main.hs
@@ -1,11 +1,11 @@
 module Main (main) where
 
 import qualified Cachix.Client as CC
-import Control.Exception (SomeException, fromException, handle)
+import Cachix.Client.Exception (CachixException)
+import Control.Exception (displayException, handle)
 import GHC.IO.Encoding
-import System.Exit (exitFailure, exitWith)
+import System.Exit (exitFailure)
 import System.IO
-import Prelude
 
 main :: IO ()
 main = do
@@ -17,10 +17,8 @@ main = do
 handleExceptions :: IO a -> IO a
 handleExceptions = handle handler
   where
-    handler :: SomeException -> IO a
-    handler e | (Just ee) <- fromException e = exitWith ee
+    handler :: CachixException -> IO a
     handler e = do
       hPutStrLn stderr ""
-      -- TODO: pretty print the record once fixed https://github.com/haskell-servant/servant/issues/807
-      hPrint stderr e
+      hPutStr stderr (displayException e)
       exitFailure

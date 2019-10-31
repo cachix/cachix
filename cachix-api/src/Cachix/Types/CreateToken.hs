55f71c7a0795d046cbe894b644a56a7a5cecaa8e
Robert Hensing <robert@roberthensing.nl>
2019-10-31 18:09:59 +0100

    cachix-api: Add token description field

---
+++ cachix-api/src/Cachix/Types/CreateToken.hs
@@ -0,0 +1,17 @@
+module Cachix.Types.CreateToken
+  ( CreateToken(..)
+  )
+where
+
+import Data.Aeson
+  ( FromJSON,
+  ToJSON
+  )
+import Data.Swagger
+import Protolude
+
+data CreateToken
+  = CreateToken
+    { description :: Text
+    }
+    deriving (Show, Generic, FromJSON, ToJSON, ToSchema)

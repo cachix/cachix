8b16fb8265bac71d985213ed4e3994da9bfd23cb
Domen Kožar <domen@dev.si>
2020-02-20 17:14:24 +0100

    fix build

--- default.nix
+++ default.nix
@@ -3,6 +3,6 @@ let
   src = pkgs.gitignoreSource ./.;
   cachix-api = pkgs.haskellPackages.callCabal2nix "cachix-api" (pkgs.gitignoreSource ./cachix-api) {};
   cachix = pkgs.haskellPackages.callCabal2nix "cachix" (pkgs.gitignoreSource ./cachix) { inherit cachix-api; };
-in cachix // {
+in pkgs.haskell.lib.disableLibraryProfiling cachix // {
   inherit (pkgs) pre-commit-check;
 }

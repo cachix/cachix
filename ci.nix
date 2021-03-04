e87a728f7fc2f99d5545c76acd7514274d5df0d6
Robert Hensing <robert@roberthensing.nl>
2021-03-04 13:29:11 +0100

    ci.nix: Optional multi-system build

--- ci.nix
+++ ci.nix
@@ -1,4 +1,20 @@
-{
-  cachix = import ./default.nix {};
-  pre-commit-check = (import ./nix {}).pre-commit-check;
-}
+{ src ? null
+, multiSystemBuild ? src != null # true for Hercules CI
+, system ? builtins.currentSystem
+}:
+let
+  ci = system: _: {
+    cachix = import ./default.nix { inherit system; };
+    pre-commit-check = (import ./nix { inherit system; }).pre-commit-check;
+    recurseForDerivations = true;
+  };
+
+  ciSystems = {
+    "x86_64-linux" = {};
+    "aarch64-linux" = {};
+    "x86_64-darwin" = {};
+  };
+in
+  if multiSystemBuild
+  then builtins.mapAttrs ci ciSystems
+  else ci system {}

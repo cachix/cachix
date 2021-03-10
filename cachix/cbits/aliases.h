6c1e6e0bbaf59c60c7ac0a3c58bbf2d6bb6596a3
Domen Kožar <domen@dev.si>
2021-03-10 10:21:13 +0000

    remove cbits

--- cachix/cbits/aliases.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-// inline-c-cpp doesn't seem to handle namespace operator or template
-// syntax so we help it a bit for now. This definition can be inlined
-// when it is supported by inline-c-cpp.
-typedef nix::ref<nix::Store> refStore;
-typedef nix::ref<const nix::ValidPathInfo> refValidPathInfo;
-typedef nix::PathSet::iterator PathSetIterator;

5808f0ec5d7cdc6c46741f1efc8e9ec68a5ad0bf
Domen Kožar <domen@dev.si>
2021-11-12 17:39:36 -0600

    bump lts

--- nix/stack-shell.nix
+++ nix/stack-shell.nix
@@ -1,8 +1,13 @@
+{ ghcVersion }:
+
 let 
   sources = (import ./sources.nix);
   pkgs = import sources.nixpkgs {} ;
 in pkgs.haskell.lib.buildStackProject {
   name = "cachix-stack-shell";
+
+  ghc = pkgs.haskell.compiler."${ghcVersion}";
+
   buildInputs = [
     pkgs.lzma
     pkgs.zlib

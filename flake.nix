f0d1e3ab1277a1915b6a279f07ab33332378d555
Domen Kožar <domen@dev.si>
2021-12-16 10:36:08 +0100

    flake: add aarch64-darwin

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "CLI for Hosted Nix binary caches";
 
   outputs = { self }: let
-    systems = [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" ];
+    systems = [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
     forAllSystems = f: builtins.listToAttrs (map (name: { inherit name; value = f name; }) systems);
   in {
     packages = forAllSystems (system: {

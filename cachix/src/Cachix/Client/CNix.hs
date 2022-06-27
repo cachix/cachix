e1f18b90d550c224e6e2a1edb65b5a333c8932a6
Domen Kožar <domen@dev.si>
2022-06-27 12:43:49 -0500

    Properly handle invalid paths

--- cachix/src/Cachix/Client/CNix.hs
+++ cachix/src/Cachix/Client/CNix.hs
@@ -1,22 +1,20 @@
 module Cachix.Client.CNix where
 
-import qualified Data.Text as T
-import qualified Language.C.Inline.Cpp.Exceptions as C
+import Hercules.CNix.Store (Store, StorePath, isValidPath, storePathToPath)
 import Protolude
 import System.Console.Pretty (Color (..), color)
 
-predicateInvalidPath :: SomeException -> Maybe Text
-predicateInvalidPath e
-  | Just (C.CppStdException msg) <- fromException e =
-    if "nix::InvalidPath" `T.isSuffixOf` toS msg
-      then Just $ toS msg
-      else Nothing
-predicateInvalidPath _ = Nothing
 
-handleInvalidPath :: IO (Maybe a) -> IO (Maybe a)
-handleInvalidPath = handleJust predicateInvalidPath handle_
-  where
-    handle_ :: Text -> IO (Maybe a)
-    handle_ msg = do
-      hPutStrLn stderr $ color Yellow $ "Warning: " <> msg
-      return Nothing
+filterInvalidStorePaths :: Store -> [StorePath] -> IO [Maybe StorePath]
+filterInvalidStorePaths store storePaths =
+  traverse (filterInvalidStorePath store) storePaths
+
+filterInvalidStorePath :: Store -> StorePath -> IO (Maybe StorePath)
+filterInvalidStorePath store storePath = do
+  isValid <- isValidPath store storePath
+  if isValid
+  then return $ Just storePath
+  else do 
+    path <- storePathToPath store storePath
+    hPutStrLn stderr $ color Yellow $ "Warning: " <> (decodeUtf8With lenientDecode $ path) <> " is not valid, skipping"
+    return Nothing

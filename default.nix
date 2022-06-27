9b887c2c185a9f81db072b1b8a423a424767fd63
Domen Kožar <domen@dev.si>
2022-06-27 00:27:11 -0500

    Simplify packaging

--- default.nix
+++ default.nix
@@ -5,21 +5,6 @@
 let
 
   cachix-api = pkgs.haskellPackages.callCabal2nix "cachix-api" (pkgs.gitignoreSource ./cachix-api) {};
-  cachix = pkgs.haskellPackages.callCabal2nix "cachix" (pkgs.gitignoreSource ./cachix) { inherit cachix-api hercules-ci-cnix-store; nix = pkgs.nix_2_4; };
+  cachix = pkgs.haskellPackages.callCabal2nix "cachix" (pkgs.gitignoreSource ./cachix) { inherit cachix-api; };
 
-  withNixUnstable =
-    import ./default.nix { pkgs = pkgs.extend (self: super: { nix = self.nixUnstable; }); };
-
-  addNixVersionFlag = pkg:
-    pkgs.haskell.lib.overrideCabal pkg (o: {
-      preConfigure = (o.preConfigure or "") + ''
-        if pkg-config --atleast-version 2.4pre nix-store; then
-          configureFlags="$configureFlags --flag nix-2_4"
-        fi
-      '';
-    });
-  hercules-ci-cnix-store = pkgs.haskell.lib.addBuildDepends (
-    addNixVersionFlag pkgs.haskellPackages.hercules-ci-cnix-store
-  ) [ pkgs.nlohmann_json ];
-
-in pkgs.haskell.lib.justStaticExecutables cachix // { inherit withNixUnstable; }
+in pkgs.haskell.lib.justStaticExecutables cachix

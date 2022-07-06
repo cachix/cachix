7cf672109f91ccb3a0e86eff63a5960eac3999d9
Domen Kožar <domen@dev.si>
2022-07-06 15:24:36 -0500

    deployment is now a separate process

--- cachix/src/Cachix/Deploy/Agent.hs
+++ cachix/src/Cachix/Deploy/Agent.hs
@@ -4,71 +4,52 @@ module Cachix.Deploy.Agent where
 
 import qualified Cachix.API.WebSocketSubprotocol as WSS
 import qualified Cachix.Client.OptionsParser as CachixOptions
-import Cachix.Client.Retry
-import qualified Cachix.Deploy.Activate as Activate
+import Cachix.Client.URI (getBaseUrl)
 import qualified Cachix.Deploy.OptionsParser as AgentOptions
+import Cachix.Deploy.StdinProcess (readProcess)
 import qualified Cachix.Deploy.Websocket as CachixWebsocket
-import Conduit ((.|))
-import qualified Control.Concurrent.Async as Async
-import qualified Control.Concurrent.STM.TQueue as TQueue
 import qualified Data.Aeson as Aeson
-import qualified Data.Conduit as Conduit
-import qualified Data.Conduit.Combinators as Conduit
-import qualified Data.Conduit.TQueue as Conduit
-import Data.IORef
-import Data.String (String)
-import Data.Time.Clock (getCurrentTime)
-import Data.UUID (UUID)
-import qualified Data.UUID as UUID
 import qualified Katip as K
-import Network.HTTP.Simple (RequestHeaders)
 import qualified Network.WebSockets as WS
+import Paths_cachix (getBinDir)
 import Protolude hiding (toS)
 import Protolude.Conv
-import qualified Wuss
+import qualified Servant.Client as Servant
 
 run :: CachixOptions.CachixOptions -> AgentOptions.AgentOptions -> IO ()
 run cachixOptions agentOpts = do
-  CachixWebsocket.runForever cachixOptions agentOpts handleMessage
+  CachixWebsocket.runForever options handleMessage
   where
-    handleMessage :: (K.KatipContextT IO () -> IO ()) -> String -> RequestHeaders -> ByteString -> WS.Connection -> CachixWebsocket.AgentState -> ByteString -> K.KatipContextT IO ()
-    handleMessage runKatip host headers payload connection agentState agentToken = do
-      case WSS.parseMessage payload of
-        (Left err) ->
-          -- TODO: show the bytestring?
-          K.logLocM K.ErrorS $ K.ls $ "Failed to parse websocket payload: " <> err
-        (Right message) ->
-          case WSS.command message of
-            WSS.AgentRegistered agentInformation -> do
-              K.logLocM K.InfoS "Agent registered."
-              liftIO $ atomicWriteIORef agentState (Just agentInformation)
-            WSS.Deployment deploymentDetails -> do
-              maybeAgentInformation <- liftIO $ readIORef agentState
-              let index :: Text
-                  index = show $ WSS.index deploymentDetails
-                  deploymentID = WSS.id (deploymentDetails :: WSS.DeploymentDetails)
-              case maybeAgentInformation of
-                Nothing -> K.logLocM K.InfoS $ K.ls $ "Ignoring deployment #" <> index <> " as agent isn't registered yet."
-                Just agentInformation -> do
-                  queue <- liftIO $ atomically TQueue.newTQueue
-                  liftIO $ Async.race_ (runLogStreaming runKatip host headers queue deploymentID) $ runKatip $ Activate.activate cachixOptions agentOpts connection (Conduit.sinkTQueue queue) deploymentDetails agentInformation agentToken
-
-    runLogStreaming :: (K.KatipContextT IO () -> IO ()) -> String -> RequestHeaders -> Conduit.TQueue ByteString -> UUID -> IO ()
-    runLogStreaming runKatip host headers queue deploymentID = do
-      -- TODO: debug Conduit.print
-      let path = "/api/v1/deploy/log/" <> UUID.toText deploymentID
-      retryAllWithLogging endlessRetryPolicy (CachixWebsocket.logger runKatip) $ do
-        liftIO $
-          Wuss.runSecureClientWith host 443 (toS path) WS.defaultConnectionOptions headers $ \connection ->
-            bracket_ (return ()) (WS.sendClose connection ("Closing." :: ByteString)) $
-              Conduit.runConduit $
-                Conduit.sourceTQueue queue
-                  .| Conduit.linesUnboundedAscii
-                  .| websocketSend connection
+    host = toS $ Servant.baseUrlHost $ getBaseUrl $ CachixOptions.host cachixOptions
+    name = AgentOptions.name agentOpts
+    options =
+      CachixWebsocket.Options
+        { CachixWebsocket.host = host,
+          CachixWebsocket.name = name,
+          CachixWebsocket.path = "/ws",
+          CachixWebsocket.profile = AgentOptions.profile agentOpts,
+          CachixWebsocket.isVerbose = CachixOptions.verbose cachixOptions
+        }
+    handleMessage :: ByteString -> (K.KatipContextT IO () -> IO ()) -> WS.Connection -> CachixWebsocket.AgentState -> ByteString -> K.KatipContextT IO ()
+    handleMessage payload _ _ agentState _ = do
+      CachixWebsocket.parseMessage payload (handleCommand . WSS.command)
       where
-        websocketSend :: WS.Connection -> Conduit.ConduitT ByteString Conduit.Void IO ()
-        websocketSend connection = Conduit.mapM_ f
-          where
-            f = \bs -> do
-              now <- getCurrentTime
-              WS.sendTextData connection $ Aeson.encode $ WSS.Log {line = toS bs, time = now}
+        handleCommand :: WSS.BackendCommand -> K.KatipContextT IO ()
+        handleCommand (WSS.AgentRegistered agentInformation) = do
+          CachixWebsocket.registerAgent agentState agentInformation
+        handleCommand (WSS.Deployment deploymentDetails) = do
+          -- TODO: lock to ensure one deployment at the time
+          let input =
+                CachixWebsocket.Input
+                  { deploymentDetails = deploymentDetails,
+                    websocketOptions =
+                      CachixWebsocket.Options
+                        { host = host,
+                          name = name,
+                          path = "/ws-deployment",
+                          profile = AgentOptions.profile agentOpts,
+                          isVerbose = CachixOptions.verbose cachixOptions
+                        }
+                  }
+          binDir <- toS <$> liftIO getBinDir
+          liftIO $ readProcess (binDir <> "/.cachix-deployment") [] (toS $ Aeson.encode input)
--- cachix/src/Cachix/Deploy/Websocket.hs
+++ cachix/src/Cachix/Deploy/Websocket.hs
@@ -1,42 +1,47 @@
 -- high level interface for websocket clients
 module Cachix.Deploy.Websocket where
 
+import Cachix.API.WebSocketSubprotocol (AgentInformation)
 import qualified Cachix.API.WebSocketSubprotocol as WSS
-import qualified Cachix.Client.OptionsParser as CachixOptions
 import Cachix.Client.Retry
-import Cachix.Client.URI (getBaseUrl)
 import Cachix.Client.Version (versionNumber)
-import qualified Cachix.Deploy.OptionsParser as AgentOptions
 import qualified Cachix.Deploy.WebsocketPong as WebsocketPong
 import qualified Control.Exception.Safe as Exception
+import Data.Aeson (FromJSON, ToJSON)
 import Data.IORef
-import Data.String (String)
 import qualified Katip as K
-import Network.HTTP.Simple (RequestHeaders)
+import Network.HTTP.Types (Header)
 import qualified Network.WebSockets as WS
 import Protolude hiding (toS)
 import Protolude.Conv
-import qualified Servant.Client as Servant
 import System.Environment (getEnv)
 import qualified Wuss
 
 type AgentState = IORef (Maybe WSS.AgentInformation)
 
--- TODO: how to pass cmd options to the worker?
--- TODO: agent name
-runForever :: CachixOptions.CachixOptions -> AgentOptions.AgentOptions -> ((K.KatipContextT IO () -> IO ()) -> String -> RequestHeaders -> ByteString -> WS.Connection -> AgentState -> ByteString -> K.KatipContextT IO ()) -> IO ()
-runForever cachixOptions agentOpts cmd = withKatip (CachixOptions.verbose cachixOptions) $ \logEnv -> do
+data Options = Options
+  { host :: Text,
+    path :: Text,
+    name :: Text,
+    isVerbose :: Bool,
+    profile :: Text
+  }
+  deriving (Show, Generic, ToJSON, FromJSON)
+
+data Input = Input
+  { deploymentDetails :: WSS.DeploymentDetails,
+    websocketOptions :: Options
+  }
+  deriving (Show, Generic, ToJSON, FromJSON)
+
+runForever :: Options -> (ByteString -> (K.KatipContextT IO () -> IO ()) -> WS.Connection -> AgentState -> ByteString -> K.KatipContextT IO ()) -> IO ()
+runForever options cmd = withKatip (isVerbose options) $ \logEnv -> do
   agentToken <- getEnv "CACHIX_AGENT_TOKEN"
   -- TODO: error if token is missing
   agentState <- newIORef Nothing
   pongState <- WebsocketPong.newState
   mainThreadID <- myThreadId
   let runKatip = K.runKatipContextT logEnv () "agent"
-      headers =
-        [ ("Authorization", "Bearer " <> toS agentToken),
-          ("name", name),
-          ("version", toS versionNumber)
-        ]
       pingHandler = do
         last <- WebsocketPong.secondsSinceLastPong pongState
         runKatip $ K.logLocM K.DebugS $ K.ls $ "Sending WebSocket keep-alive ping, last pong was " <> (show last :: Text) <> " seconds ago"
@@ -44,24 +49,28 @@ runForever cachixOptions agentOpts cmd = withKatip (CachixOptions.verbose cachix
       connectionOptions = WebsocketPong.installPongHandler pongState WS.defaultConnectionOptions
   runKatip $
     retryAllWithLogging endlessRetryPolicy (logger runKatip) $ do
-      K.logLocM K.InfoS $ K.ls ("Agent " <> agentIdentifier <> " connecting to " <> toS host <> toS path)
+      K.logLocM K.InfoS $ K.ls ("Agent " <> agentIdentifier <> " connecting to " <> toS (host options) <> toS (path options))
       liftIO $ do
         -- refresh pong state in case we're reconnecting
         WebsocketPong.pongHandler pongState
-        Wuss.runSecureClientWith host 443 path connectionOptions headers $ \connection -> runKatip $ do
+        Wuss.runSecureClientWith (toS $ host options) 443 (toS $ path options) connectionOptions (headers options (toS agentToken)) $ \connection -> runKatip $ do
           K.logLocM K.InfoS "Connected to Cachix Deploy service"
           liftIO $
             WS.withPingThread connection pingEvery pingHandler $ do
               WSS.recieveDataConcurrently
                 connection
-                (\message -> Exception.handle (handler runKatip) $ runKatip (cmd runKatip host headers message connection agentState (toS agentToken)))
+                (\message -> Exception.handle (handler runKatip) $ runKatip (cmd message runKatip connection agentState (toS agentToken)))
   where
-    agentIdentifier = name <> " " <> toS versionNumber
-    name = toS (AgentOptions.name agentOpts)
-    path = "/ws"
+    agentIdentifier = name options <> " " <> toS versionNumber
     pingEvery = 30
     pongTimeout = pingEvery * 2
-    host = Servant.baseUrlHost $ getBaseUrl $ CachixOptions.host cachixOptions
+
+headers :: Options -> ByteString -> [Header]
+headers options agentToken =
+  [ ("Authorization", "Bearer " <> toS agentToken),
+    ("name", toS (name options)),
+    ("version", toS versionNumber)
+  ]
 
 -- TODO: log the exception
 handler :: (K.KatipContextT IO () -> IO ()) -> Exception.SomeException -> IO ()
@@ -79,3 +88,19 @@ withKatip isVerbose =
       logEnv <- K.initLogEnv "agent" "production"
       stdoutScribe <- K.mkHandleScribe K.ColorIfTerminal stdout (K.permitItem permit) K.V2
       K.registerScribe "stdout" stdoutScribe K.defaultScribeSettings logEnv
+
+parseMessage :: FromJSON cmd => ByteString -> (WSS.Message cmd -> K.KatipContextT IO ()) -> K.KatipContextT IO ()
+parseMessage payload m = do
+  case WSS.parseMessage payload of
+    (Left err) ->
+      -- TODO: show the bytestring?
+      K.logLocM K.ErrorS $ K.ls $ "Failed to parse websocket payload: " <> err
+    (Right message) ->
+      m message
+
+-- commands
+
+registerAgent :: AgentState -> AgentInformation -> K.KatipContextT IO ()
+registerAgent agentState agentInformation = do
+  K.logLocM K.InfoS "Agent registered."
+  liftIO $ atomicWriteIORef agentState (Just agentInformation)

8109fecd7d17fb5d596144d534c1c5322387d06e
sandydoo <hey@sandydoo.me>
2022-09-04 14:39:31 +0000

    Move agent state and token creation out of the websocket
    - Change the token from a bytestring to text.

--- cachix/src/Cachix/Deploy/Agent.hs
+++ cachix/src/Cachix/Deploy/Agent.hs
@@ -5,31 +5,74 @@ module Cachix.Deploy.Agent where
 import qualified Cachix.API.WebSocketSubprotocol as WSS
 import qualified Cachix.Client.Config as Config
 import Cachix.Client.URI (getBaseUrl)
+import Cachix.Client.Version (versionNumber)
 import qualified Cachix.Deploy.Log as Log
 import qualified Cachix.Deploy.OptionsParser as AgentOptions
 import Cachix.Deploy.StdinProcess (readProcess)
 import qualified Cachix.Deploy.Websocket as WebSocket
+import Control.Exception.Safe (handleAny, onException)
 import qualified Data.Aeson as Aeson
+import Data.IORef
+import Data.String (String)
 import qualified Katip as K
 import qualified Network.WebSockets as WS
 import Paths_cachix (getBinDir)
-import Protolude hiding (toS)
+import Protolude hiding (onException, toS)
 import Protolude.Conv
 import qualified Servant.Client as Servant
+import qualified System.Directory as Directory
+import System.Environment (getEnv, lookupEnv)
+import qualified System.Posix.Files as Posix.Files
+import qualified System.Posix.User as Posix.User
+
+type AgentState = IORef (Maybe WSS.AgentInformation)
 
 data Deployment = Deployment
-  { profileName :: Text,
+  { agentName :: Text,
+    agentToken :: Text,
+    profileName :: Text,
+    host :: Text,
     deploymentDetails :: WSS.DeploymentDetails,
-    logOptions :: Log.Options,
-    websocketOptions :: WebSocket.Options
+    logOptions :: Log.Options
   }
   deriving (Show, Generic, Aeson.ToJSON, Aeson.FromJSON)
 
+registerAgent :: AgentState -> WSS.AgentInformation -> K.KatipContextT IO ()
+registerAgent agentState agentInformation = do
+  K.logLocM K.InfoS "Agent registered."
+  liftIO $ atomicWriteIORef agentState (Just agentInformation)
+
+agentIdentifier :: Text -> Text
+agentIdentifier agentName = agentName <> " " <> toS versionNumber
+
 run :: Config.CachixOptions -> AgentOptions.AgentOptions -> IO ()
 run cachixOptions agentOpts =
-  Log.withLog logOptions $ \withLog -> do
-    WebSocket.runForever withLog options (handleMessage withLog)
+  Log.withLog logOptions $ \withLog ->
+    handleAny (logAndExit withLog) $ do
+      checkUserOwnsHome
+
+      -- TODO: error if token is missing
+      agentToken <- toS <$> getEnv "CACHIX_AGENT_TOKEN"
+      agentState <- newIORef Nothing
+
+      let agentName = AgentOptions.name agentOpts
+      let websocketOptions =
+            WebSocket.Options
+              { WebSocket.host = host,
+                WebSocket.path = "/ws",
+                WebSocket.headers = WebSocket.createHeaders agentName agentToken,
+                WebSocket.agentIdentifier = agentIdentifier agentName
+              }
+
+      WebSocket.runForever withLog websocketOptions (handleMessage withLog agentState agentName agentToken)
   where
+    host = toS $ Servant.baseUrlHost $ getBaseUrl $ Config.host cachixOptions
+    profileName = fromMaybe "system" (AgentOptions.profile agentOpts)
+
+    logAndExit withLog e = do
+      void $ withLog $ K.logLocM K.ErrorS $ K.ls (displayException e)
+      exitFailure
+
     verbosity =
       if Config.verbose cachixOptions
         then Log.Verbose
@@ -42,18 +85,8 @@ run cachixOptions agentOpts =
           environment = "production"
         }
 
-    host = toS $ Servant.baseUrlHost $ getBaseUrl $ Config.host cachixOptions
-    name = AgentOptions.name agentOpts
-    profileName = fromMaybe "system" (AgentOptions.profile agentOpts)
-    options =
-      WebSocket.Options
-        { WebSocket.host = host,
-          WebSocket.name = name,
-          WebSocket.path = "/ws"
-        }
-
-    handleMessage :: Log.WithLog -> WS.Connection -> ByteString -> WebSocket.AgentState -> ByteString -> IO ()
-    handleMessage withLog _ payload agentState _ =
+    handleMessage :: Log.WithLog -> AgentState -> Text -> Text -> WS.Connection -> ByteString -> IO ()
+    handleMessage withLog agentState agentName agentToken _ payload =
       case WSS.parseMessage payload of
         Left err ->
           -- TODO: show the bytestring?
@@ -63,19 +96,56 @@ run cachixOptions agentOpts =
       where
         handleCommand :: WSS.BackendCommand -> IO ()
         handleCommand (WSS.AgentRegistered agentInformation) =
-          withLog $ WebSocket.registerAgent agentState agentInformation
+          withLog $ registerAgent agentState agentInformation
         handleCommand (WSS.Deployment deploymentDetails) = do
           binDir <- toS <$> getBinDir
           readProcess (binDir <> "/.cachix-deployment") [] $
             toS . Aeson.encode $
               Deployment
-                { profileName = profileName,
+                { agentName = agentName,
+                  agentToken = agentToken,
+                  profileName = profileName,
+                  host = host,
                   deploymentDetails = deploymentDetails,
-                  logOptions = logOptions,
-                  websocketOptions =
-                    WebSocket.Options
-                      { host = host,
-                        name = name,
-                        path = "/ws-deployment"
-                      }
+                  logOptions = logOptions
                 }
+
+-- | Fetch the home directory and verify that the owner matches the current user.
+-- Throws either 'NoHomeFound' or 'UserDoesNotOwnHome'.
+checkUserOwnsHome :: IO ()
+checkUserOwnsHome = do
+  home <- Directory.getHomeDirectory `onException` throwIO NoHomeFound
+  stat <- Posix.Files.getFileStatus home
+  userId <- Posix.User.getEffectiveUserID
+
+  when (userId /= Posix.Files.fileOwner stat) $ do
+    userName <- Posix.User.userName <$> Posix.User.getUserEntryForID userId
+    sudoUser <- lookupEnv "SUDO_USER"
+    throwIO $
+      UserDoesNotOwnHome
+        { userName = userName,
+          sudoUser = sudoUser,
+          home = home
+        }
+
+data Error
+  = -- | No home directory.
+    NoHomeFound
+  | -- | Safeguard against creating root-owned files in user directories.
+    -- This is an issue on macOS, where, by default, sudo does not reset $HOME.
+    UserDoesNotOwnHome
+      { userName :: String,
+        sudoUser :: Maybe String,
+        home :: FilePath
+      }
+  deriving (Show)
+
+instance Exception Error where
+  displayException NoHomeFound = "Could not find the user’s home directory. Make sure to set the $HOME variable."
+  displayException UserDoesNotOwnHome {userName = userName, sudoUser = sudoUser, home = home} =
+    if isJust sudoUser
+      then toS $ unlines [warningMessage, suggestSudoFlagH]
+      else toS warningMessage
+    where
+      warningMessage = "The current user (" <> toS userName <> ") does not own the home directory (" <> toS home <> ")"
+      suggestSudoFlagH = "Try running the agent with `sudo -H`."
--- cachix/src/Cachix/Deploy/Websocket.hs
+++ cachix/src/Cachix/Deploy/Websocket.hs
@@ -1,38 +1,32 @@
 -- high level interface for websocket clients
 module Cachix.Deploy.Websocket where
 
-import Cachix.API.WebSocketSubprotocol (AgentInformation)
 import qualified Cachix.API.WebSocketSubprotocol as WSS
 import Cachix.Client.Retry
 import Cachix.Client.Version (versionNumber)
 import qualified Cachix.Deploy.Log as Log
 import qualified Cachix.Deploy.WebsocketPong as WebsocketPong
-import Control.Exception.Safe (catchAny, onException)
 import Control.Retry (RetryStatus (..))
 import qualified Control.Retry as Retry
-import Data.Aeson (FromJSON, ToJSON)
-import Data.IORef
+import qualified Data.Aeson as Aeson
 import Data.String (String)
 import qualified Katip as K
 import Network.HTTP.Types (Header)
 import qualified Network.WebSockets as WS
-import Protolude hiding (catch, onException, toS)
+import Protolude hiding (toS)
 import Protolude.Conv
-import qualified System.Directory as Directory
-import System.Environment (getEnv, lookupEnv)
 import qualified System.Info
-import qualified System.Posix.Files as Posix.Files
-import qualified System.Posix.User as Posix.User
 import qualified Wuss
-
-type AgentState = IORef (Maybe WSS.AgentInformation)
 
 data Options = Options
   { host :: Text,
     path :: Text,
-    name :: Text
+    headers :: [Header],
+    -- | The identifier used when logging. Usually a combination of the agent
+    -- name and the CLI version.
+    agentIdentifier :: Text
   }
-  deriving (Show, Generic, ToJSON, FromJSON)
+  deriving (Show)
 
 system :: String
 system = System.Info.arch <> "-" <> System.Info.os
@@ -42,53 +36,48 @@ runForever ::
   Log.WithLog ->
   -- | WebSocket options
   Options ->
-  (WS.Connection -> ByteString -> AgentState -> ByteString -> IO ()) ->
+  -- | The app to run inside the socket
+  (WS.Connection -> ByteString -> IO ()) ->
   IO ()
 runForever withLog options inner = do
-  checkUserOwnsHome `catchAny` \e -> do
-    withLog $ K.logLocM K.ErrorS $ K.ls (displayException e)
-    exitFailure
-
-  -- TODO: error if token is missing
-  agentToken <- getEnv "CACHIX_AGENT_TOKEN"
-  agentState <- newIORef Nothing
-
-  pongState <- WebsocketPong.newState
   mainThreadID <- myThreadId
+  pongState <- WebsocketPong.newState
 
+  let pingEvery = 30
+  let pongTimeout = pingEvery * 2
   let pingHandler = do
         last <- WebsocketPong.secondsSinceLastPong pongState
         withLog $ K.logLocM K.DebugS $ K.ls $ "Sending WebSocket keep-alive ping, last pong was " <> (show last :: Text) <> " seconds ago"
         WebsocketPong.pingHandler pongState mainThreadID pongTimeout
-      connectionOptions = WebsocketPong.installPongHandler pongState WS.defaultConnectionOptions
+  let connectionOptions = WebsocketPong.installPongHandler pongState WS.defaultConnectionOptions
 
   -- TODO: use exponential retry with reset: https://github.com/Soostone/retry/issues/25
   -- retryWithLogging endlessConstantRetryPolicy (logRetry withLog) $ do
-  withLog $ K.logLocM K.InfoS $ K.ls ("Agent " <> agentIdentifier <> " connecting to " <> toS (host options) <> toS (path options))
+  withLog $
+    K.logLocM K.InfoS $ K.ls $ "Agent " <> agentIdentifier options <> " connecting to " <> host options <> path options
 
   -- refresh pong state in case we're reconnecting
   WebsocketPong.pongHandler pongState
 
   -- TODO: https://github.com/jaspervdj/websockets/issues/229
-  Wuss.runSecureClientWith (toS $ host options) 443 (toS $ path options) connectionOptions (headers options (toS agentToken)) $ \connection -> do
+  Wuss.runSecureClientWith (toS $ host options) 443 (toS $ path options) connectionOptions (headers options) $ \connection -> do
     withLog $ K.logLocM K.InfoS "Connected to Cachix Deploy service"
     WS.withPingThread connection pingEvery pingHandler $
-      WSS.recieveDataConcurrently
-        connection
-        (\message -> inner connection message agentState (toS agentToken))
-  where
-    agentIdentifier = name options <> " " <> toS versionNumber
-    pingEvery = 30
-    pongTimeout = pingEvery * 2
+      WSS.recieveDataConcurrently connection (inner connection)
 
 exitWithCloseRequest :: WS.ConnectionException -> IO ()
 exitWithCloseRequest (WS.CloseRequest _ _) = return ()
 exitWithCloseRequest e = throwIO e
 
-headers :: Options -> ByteString -> [Header]
-headers options agentToken =
+createHeaders ::
+  -- | Agent name
+  Text ->
+  -- | Agent Token
+  Text ->
+  [Header]
+createHeaders agentName agentToken =
   [ ("Authorization", "Bearer " <> toS agentToken),
-    ("name", toS (name options)),
+    ("name", toS agentName),
     ("version", toS versionNumber),
     ("system", toS system)
   ]
@@ -104,7 +93,7 @@ logRetry withLog _ exception retryStatus =
 
 -- TODO: Move to the agent. If the websocket is going to parse messages then it
 -- can’t also swallow errors like this.
-parseMessage :: K.KatipContext m => FromJSON cmd => ByteString -> (WSS.Message cmd -> m ()) -> m ()
+parseMessage :: K.KatipContext m => Aeson.FromJSON cmd => ByteString -> (WSS.Message cmd -> m ()) -> m ()
 parseMessage payload m = do
   case WSS.parseMessage payload of
     Left err ->
@@ -112,50 +101,3 @@ parseMessage payload m = do
       K.logLocM K.ErrorS $ K.ls $ "Failed to parse websocket payload: " <> err
     Right message ->
       m message
-
--- commands
-
-registerAgent :: AgentState -> AgentInformation -> K.KatipContextT IO ()
-registerAgent agentState agentInformation = do
-  K.logLocM K.InfoS "Agent registered."
-  liftIO $ atomicWriteIORef agentState (Just agentInformation)
-
--- | Fetch the home directory and verify that the owner matches the current user.
--- Throws either 'NoHomeFound' or 'UserDoesNotOwnHome'.
-checkUserOwnsHome :: IO ()
-checkUserOwnsHome = do
-  home <- Directory.getHomeDirectory `onException` throwIO NoHomeFound
-  stat <- Posix.Files.getFileStatus home
-  userId <- Posix.User.getEffectiveUserID
-
-  when (userId /= Posix.Files.fileOwner stat) $ do
-    userName <- Posix.User.userName <$> Posix.User.getUserEntryForID userId
-    sudoUser <- lookupEnv "SUDO_USER"
-    throwIO $
-      UserDoesNotOwnHome
-        { userName = userName,
-          sudoUser = sudoUser,
-          home = home
-        }
-
-data Error
-  = -- | No home directory.
-    NoHomeFound
-  | -- | Safeguard against creating root-owned files in user directories.
-    -- This is an issue on macOS, where, by default, sudo does not reset $HOME.
-    UserDoesNotOwnHome
-      { userName :: String,
-        sudoUser :: Maybe String,
-        home :: FilePath
-      }
-  deriving (Show)
-
-instance Exception Error where
-  displayException NoHomeFound = "Could not find the user’s home directory. Make sure to set the $HOME variable."
-  displayException UserDoesNotOwnHome {userName = userName, sudoUser = sudoUser, home = home} =
-    if isJust sudoUser
-      then toS $ unlines [warningMessage, suggestSudoFlagH]
-      else toS warningMessage
-    where
-      warningMessage = "The current user (" <> toS userName <> ") does not own the home directory (" <> toS home <> ")"
-      suggestSudoFlagH = "Try running the agent with `sudo -H`."

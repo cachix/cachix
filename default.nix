51f705c6b8c08a31e97da16c9645d0b2e67782da
Domen Kožar <domen@dev.si>
2022-12-18 13:48:53 +0000

    flakeify the project

--- default.nix
+++ default.nix
@@ -1,17 +1,10 @@
-{ system ? builtins.currentSystem
-, pkgs ? import ./nix { inherit system; }
-}:
-let
-  # TODO: Choose between stack and cabal. Don't use both!
-  # GHC version should match nixpkgs to test potential release build
-  haskellPackages = pkgs.haskellPackages;
-
-  cachix-api = haskellPackages.callCabal2nix "cachix-api" (pkgs.gitignoreSource ./cachix-api) {};
-
-  cachix = haskellPackages.callCabal2nix "cachix" (pkgs.gitignoreSource ./cachix) {
-    inherit cachix-api;
-    # Make sure that the nix version here is within the range supported by hercules-ci-cnix-store
-    nix = pkgs.nixVersions.nix_2_9;
-  };
-in
-pkgs.haskell.lib.justStaticExecutables cachix
+(import
+  (
+    let lock = builtins.fromJSON (builtins.readFile ./flake.lock); in
+    fetchTarball {
+      url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+      sha256 = lock.nodes.flake-compat.locked.narHash;
+    }
+  )
+  { src = ./.; }
+).defaultNix
--- flake.nix
+++ flake.nix
@@ -1,17 +1,62 @@
 {
   description = "CLI for Hosted Nix binary caches";
 
-  outputs = { self }: let
+  inputs = {
+    devenv.url = "github:cachix/devenv";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: let
     systems = [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
     forAllSystems = f: builtins.listToAttrs (map (name: { inherit name; value = f name; }) systems);
+    # TODO: get from nixpkgs
+    getNix = pkgs: pkgs.nixVersions.nix_2_9;
   in
     {
-      packages = forAllSystems (
-        system: {
-          cachix = import ./. { inherit system; };
+      packages = forAllSystems (system: 
+        let
+          pkgs = import nixpkgs { inherit system; };
+          
+          cachix-api = pkgs.haskellPackages.callCabal2nix "cachix-api" ./cachix-api {};
+          cachix = pkgs.haskellPackages.callCabal2nix "cachix" ./cachix {
+            inherit cachix-api;
+            nix = getNix pkgs;
+          };
+        in
+        {
+          cachix = pkgs.haskell.lib.justStaticExecutables cachix;
+          default = pkgs.haskell.lib.justStaticExecutables cachix;
+          ci = self.devShell.${system}.ci;
+        });
+
+      devShell = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in inputs.devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [{
+            packages = [ 
+              pkgs.lzma
+              pkgs.zlib
+              pkgs.boost
+              pkgs.stack
+              # sync with stack.yaml LTS
+              pkgs.haskell.compiler.ghc925
+              (getNix pkgs)
+            ];
+
+            pre-commit.hooks = {
+              cabal-fmt.enable = true;
+              hlint.enable = true;
+              ormolu.enable = true;
+              ormolu.excludes = [ "Cachix/Client/Servant\.hs$" "Cachix/Client/Config/Orphans\.hs$" "Cachix/Types/SwaggerOrphans\.hs$" ];
+              shellcheck.enable = true;
+            };
+        }];
         }
       );
-
-      defaultPackage = forAllSystems (system: self.packages.${system}.cachix);
     };
 }

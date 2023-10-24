c135d6072e0fa4543752d5a3da937484f547c932
Sander <hey@sandydoo.me>
2023-10-24 12:15:25 +0000

    daemon: load the typical user nix.conf files
    When NIX_USER_CONF_FILES is set, nix will only load the confs specified
    by it. For our use-case, we want the client to load the same user
    nix.confs that it normally does. To make that happen we need to
    explicity add the default locations for all the various confs and
    replicate the logic that nix uses.

--- cachix/src/Cachix/Client/Daemon/PostBuildHook.hs
+++ cachix/src/Cachix/Client/Daemon/PostBuildHook.hs
@@ -5,6 +5,12 @@ module Cachix.Client.Daemon.PostBuildHook where
 
 import Data.String.Here
 import Protolude
+import System.Directory
+  ( XdgDirectory (XdgConfig),
+    XdgDirectoryList (XdgConfigDirs),
+    getXdgDirectory,
+    getXdgDirectoryList,
+  )
 import System.Environment (getExecutablePath, lookupEnv)
 import System.FilePath ((</>))
 import System.IO.Temp (withSystemTempDirectory)
@@ -18,14 +24,52 @@ withSetup mdaemonSock f =
         daemonSock = fromMaybe (tempDir </> "daemon.sock") mdaemonSock
 
     cachixBin <- getExecutablePath
-    nixUserConfFiles <- lookupEnv "NIX_USER_CONF_FILES"
-    let newNixUserConfFiles = foldMap identity $ intersperse ":" $ catMaybes [nixUserConfFiles, Just postBuildHookConfigPath]
 
     writeFile postBuildHookScriptPath (postBuildHookScript cachixBin daemonSock)
     setFileMode postBuildHookScriptPath 0o755
     writeFile postBuildHookConfigPath (postBuildHookConfig postBuildHookScriptPath)
 
-    f daemonSock (toS newNixUserConfFiles)
+    nixUserConfFilesEnv <- buildNixUserConfFilesEnv postBuildHookConfigPath
+
+    f daemonSock nixUserConfFilesEnv
+
+-- | Build the NIX_USER_CONF_FILES environment variable.
+--
+-- From man nix.conf:
+--
+-- If NIX_USER_CONF_FILES is set, then each path separated by : will be loaded in reverse order.
+--
+-- Otherwise it will look for nix/nix.conf files in XDG_CONFIG_DIRS and XDG_CONFIG_HOME. If
+-- unset, XDG_CONFIG_DIRS defaults to /etc/xdg, and XDG_CONFIG_HOME defaults to $HOME/.config
+-- as per XDG Base Directory Specification.
+--
+-- We don't need to load the system config from $NIX_CONF_DIR/nix.conf.
+-- Nix loads it by default and uses it as the base config.
+buildNixUserConfFilesEnv :: FilePath -> IO Text
+buildNixUserConfFilesEnv nixConfPath = do
+  -- A user can set NIX_USER_CONF_FILES to override the default nix.conf files.
+  -- In that case, we reuse it and prepend our own config file.
+  mexistingEnv <- lookupEnv "NIX_USER_CONF_FILES"
+
+  case mexistingEnv of
+    Just existingEnv -> return $ toS $ nixConfPath <> ":" <> existingEnv
+    Nothing -> do
+      userConfigFiles <- getUserConfigFiles
+
+      -- Combine all the nix.conf paths into one string, separated by colons.
+      -- Filter out empty paths.
+      return $ toS $ intercalate ":" $ filter (not . null) $ nixConfPath : userConfigFiles
+
+getUserConfigFiles :: IO [FilePath]
+getUserConfigFiles =
+  fmap (</> "nix/nix.conf") <$> getUserConfigDirs
+
+getUserConfigDirs :: IO [FilePath]
+getUserConfigDirs = do
+  configHome <- getXdgDirectory XdgConfig empty
+  configDirs <- getXdgDirectoryList XdgConfigDirs
+
+  return $ configHome : configDirs
 
 postBuildHookConfig :: FilePath -> Text
 postBuildHookConfig scriptPath =

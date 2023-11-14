cb03d622819286422640df2b5c1217a42a80cd5d
Sander <hey@sandydoo.me>
2023-11-14 21:25:24 +0000

    daemon: cleanup

--- cachix/src/Cachix/Client/Daemon/Worker.hs
+++ cachix/src/Cachix/Client/Daemon/Worker.hs
@@ -7,7 +7,6 @@ module Cachix.Client.Daemon.Worker
 where
 
 import Cachix.Client.Config.Orphans ()
-import Cachix.Client.Daemon.ShutdownLatch
 import Cachix.Client.Daemon.Types (Daemon, DaemonEnv (..), QueuedPushRequest)
 import qualified Control.Concurrent.Async as Async
 import Control.Concurrent.STM.TBMQueue
@@ -19,17 +18,17 @@ startWorkers :: Int -> (QueuedPushRequest -> Daemon ()) -> Daemon [Immortal.Thre
 startWorkers numWorkers f = do
   replicateM numWorkers (startWorker f)
 
-stopWorkers :: [Immortal.Thread] -> Daemon ()
-stopWorkers workers = do
-  Katip.logFM Katip.InfoS "Waiting for workers to finish..."
-  liftIO $ Async.mapConcurrently_ stopWorker workers
-  Katip.logFM Katip.InfoS "Workers finished."
-
 startWorker :: (QueuedPushRequest -> Daemon ()) -> Daemon Immortal.Thread
 startWorker f = do
   Immortal.createWithLabel "worker" $ \thread -> do
     Katip.katipAddNamespace "worker" $
       Immortal.onUnexpectedFinish thread logWorkerException (runWorker f)
+
+stopWorkers :: [Immortal.Thread] -> Daemon ()
+stopWorkers workers = do
+  Katip.logFM Katip.DebugS "Waiting for workers to finish..."
+  liftIO $ Async.mapConcurrently_ stopWorker workers
+  Katip.logFM Katip.DebugS "Workers finished."
 
 stopWorker :: Immortal.Thread -> IO ()
 stopWorker worker = do

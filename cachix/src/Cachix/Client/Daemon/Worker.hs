a10a04db69a6ac6e52ab41a6a6fc1cc4db269ce7
Sander <hey@sandydoo.me>
2023-12-14 18:34:59 +0000

    daemon: generalize the worker monad

--- cachix/src/Cachix/Client/Daemon/Worker.hs
+++ cachix/src/Cachix/Client/Daemon/Worker.hs
@@ -7,40 +7,49 @@ module Cachix.Client.Daemon.Worker
 where
 
 import Cachix.Client.Config.Orphans ()
-import Cachix.Client.Daemon.Types (Daemon)
 import qualified Control.Concurrent.Async as Async
 import Control.Concurrent.STM.TBMQueue
 import qualified Control.Immortal as Immortal
+import Control.Monad.IO.Unlift (MonadUnliftIO)
 import qualified Katip
 import Protolude
 
-startWorkers :: Int -> TBMQueue a -> (a -> Daemon ()) -> Daemon [Immortal.Thread]
+startWorkers ::
+  (MonadUnliftIO m, Katip.KatipContext m) =>
+  Int ->
+  TBMQueue a ->
+  (a -> m ()) ->
+  m [Immortal.Thread]
 startWorkers numWorkers queue f = do
   replicateM numWorkers (startWorker queue f)
 
-startWorker :: TBMQueue a -> (a -> Daemon ()) -> Daemon Immortal.Thread
+startWorker ::
+  (MonadUnliftIO m, Katip.KatipContext m) =>
+  TBMQueue a ->
+  (a -> m ()) ->
+  m Immortal.Thread
 startWorker queue f = do
   Immortal.createWithLabel "worker" $ \thread -> do
     Katip.katipAddNamespace "worker" $
       Immortal.onUnexpectedFinish thread logWorkerException (runWorker queue f)
 
-stopWorkers :: [Immortal.Thread] -> Daemon ()
+stopWorkers :: (Katip.KatipContext m) => [Immortal.Thread] -> m ()
 stopWorkers workers = do
   Katip.logFM Katip.DebugS "Waiting for workers to finish..."
   liftIO $ Async.mapConcurrently_ stopWorker workers
   Katip.logFM Katip.DebugS "Workers finished."
 
-stopWorker :: Immortal.Thread -> IO ()
-stopWorker worker = do
+stopWorker :: (MonadIO m) => Immortal.Thread -> m ()
+stopWorker worker = liftIO $ do
   Immortal.mortalize worker
   Immortal.wait worker
 
-logWorkerException :: (Exception e) => Either e () -> Daemon ()
+logWorkerException :: (Exception e, Katip.KatipContext m) => Either e () -> m ()
 logWorkerException (Left err) =
   Katip.logFM Katip.ErrorS $ Katip.ls (toS $ displayException err :: Text)
 logWorkerException _ = return ()
 
-runWorker :: TBMQueue a -> (a -> Daemon ()) -> Daemon ()
+runWorker :: (MonadIO m) => TBMQueue a -> (a -> m ()) -> m ()
 runWorker queue f = loop
   where
     loop = do

f8f8e18292cbafe2cf079481766e8e78a6a65966
Sander <hey@sandydoo.me>
2024-10-03 19:38:07 +0400

    dev: use hnix-store-core 0.8

--- flake.nix
+++ flake.nix
@@ -41,7 +41,7 @@
       cachix = haskellPackages.callCabal2nix "cachix" ./cachix {
         inherit cachix-api;
         fsnotify = haskellPackages.fsnotify_0_4_1_0 or haskellPackages.fsnotify;
-        hnix-store-core = haskellPackages.hnix-store-core;
+        hnix-store-core = haskellPackages.hnix-store-core_0_8_0_0 or haskellPackages.hnix-store-core;
         nix = getNix { inherit pkgs haskellPackages; };
       };
     };

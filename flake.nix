01695c3fe42829dc3fa82aeed7e02e78f3034c35
Sander <hey@sandydoo.me>
2025-08-28 15:23:48 +0200

    ci: wire up tests against latest GHC
    HEAD is probably a bit too new and we'll likely get tons of failures from our dependencies, but lets see what happens.

--- flake.nix
+++ flake.nix
@@ -29,40 +29,69 @@
     systems = [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
     forAllSystems = nixpkgs.lib.genAttrs systems;
 
-    # Keep in sync with stack.yaml
-    ghcVersion = "98";
+    # Keep the stable version in sync with:
+    #   - the stackage release in nixpkgs
+    #   - the GHC version in nixpkgs-<stable>
+    stableGhcVersion = "98";
+    # Test builds against the latest GHC version in nixpkgs
+    latestGhcVersion = "HEAD";
 
     # Try to use the same Nix version as cnix-store, if available.
     getNix = { pkgs, haskellPackages ? pkgs.haskellPackages }:
       haskellPackages.hercules-ci-cnix-store.nixPackage or pkgs.nix;
 
+    # Build our packages with overrides applied.
+    # Provide `haskellPackages` to override the GHC version.
     customHaskellPackages = { pkgs, haskellPackages }: rec {
       cachix-api = haskellPackages.callCabal2nix "cachix-api" ./cachix-api {};
+
       cachix = haskellPackages.callCabal2nix "cachix" ./cachix {
         inherit cachix-api;
         hnix-store-core = haskellPackages.hnix-store-core_0_8_0_0 or haskellPackages.hnix-store-core;
         nix = getNix { inherit pkgs haskellPackages; };
       };
     };
+
+    mkPackages = { system, ghcVersion, suffix ? "" }:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        inherit (pkgs) lib;
+        hlib = pkgs.haskell.lib;
+        # Use haskell packages for the specified GHC version
+        haskellPackages = pkgs.haskell.packages."ghc${ghcVersion}";
+        inherit (customHaskellPackages { inherit pkgs; inherit haskellPackages; }) cachix cachix-api;
+        cachixPackages = {
+          inherit cachix-api;
+          cachix = hlib.justStaticExecutables cachix;
+        };
+        releasePackages = {
+          cachix = hlib.sdistTarball cachix;
+          cachix-api = hlib.sdistTarball cachix-api;
+        };
+        mapAttrNames = f: xs: lib.mapAttrs' (name: value: lib.nameValuePair (f name) value) xs;
+      in mapAttrNames (name: name + suffix) {
+        inherit (cachixPackages) cachix cachix-api;
+        release = pkgs.symlinkJoin {
+          name = "cachix-release" + suffix;
+          paths = lib.attrValues releasePackages;
+        };
+        all-packages = pkgs.symlinkJoin {
+          name = "cachix-packages" + suffix;
+          paths = lib.attrValues cachixPackages;
+        };
+      };
   in
     {
       packages = forAllSystems (system:
         let
-          pkgs = nixpkgs.legacyPackages.${system};
-          hlib = pkgs.haskell.lib;
-          inherit (customHaskellPackages { inherit pkgs; inherit (pkgs) haskellPackages; })
-            cachix cachix-api;
-          release = {
-            cachix = hlib.sdistTarball cachix;
-            cachix-api = hlib.sdistTarball cachix-api;
-          };
+          stablePackages = mkPackages { inherit system; ghcVersion = stableGhcVersion; };
+          latestPackages = mkPackages { inherit system; ghcVersion = latestGhcVersion; suffix = "-ghc-latest"; };
         in
-        {
-          cachix = pkgs.haskell.lib.justStaticExecutables cachix;
-          default = pkgs.haskell.lib.justStaticExecutables cachix;
-          ci = self.devShells.${system}.default.ci;
-          release = pkgs.symlinkJoin { name = "release"; paths = builtins.attrValues release; };
-        });
+          stablePackages // latestPackages // {
+            ci = self.devShells.${system}.default.ci;
+            default = stablePackages.cachix;
+          }
+      );
 
       checks = forAllSystems (system: {
         pre-commit-check = git-hooks.lib.${system}.run {
@@ -79,7 +108,7 @@
           default = inputs.devenv.lib.mkShell {
             inherit inputs pkgs;
             modules = [
-              ({ _module.args = { inherit ghcVersion getNix; }; })
+              ({ _module.args = { inherit getNix; ghcVersion = stableGhcVersion; }; })
               ./devenv.nix
             ];
           };
